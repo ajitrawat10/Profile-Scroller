bfeea905e48658582966340d0295198e514ecf43
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-03-20 12:09:18 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,23 @@ const data=[
     location:'Miami',  
     image:'https://randomuser.me/api/portraits/women/82.jpg'
     },
+ {name :'William Johnson',
+    age:'30',
+    gender:'male',
+    location:'Lynn',  
+    image:'https://randomuser.me/api/portraits/men/83.jpg'
+    },
+    {name :'James Warner',
+    age:'28',
+    gender:'male',
+    location:'London',  
+    image:'https://randomuser.me/api/portraits/men/84.jpg'
+    },
+    {name :'Maria Lammer',
+    age:'28',
+    gender:'female',
+    location:'California',  
+    image:'https://randomuser.me/api/portraits/women/84.jpg'
+    }
+
+]
