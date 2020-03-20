40119ec02b7fc2ad4de1dd1712240b1e31bf3ded
AJIT-SINGH-RAWAT <47390463+AJIT-SINGH-RAWAT@users.noreply.github.com>
2020-03-20 12:08:57 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 
+const data=[
+    {name :'John Doe',
+    age:'32',
+    gender:'male',
+    lookingfor:'female',
+    location:'Boston',  
+    image:'https://randomuser.me/api/portraits/men/82.jpg'
+    },
+    {name :'Jen Smith',
+    age:'26',
+    gender:'female',
+    location:'Miami',  
+    image:'https://randomuser.me/api/portraits/women/82.jpg'
+    },
